9b4aab8f0b48956b6566f67f8a3dacaa7eddcfd0
selvacodes <selvaganapathy@outlook.in>
2017-04-28 19:29:43 +0530

    second

---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+ import {Jumbotron} from 'react-bootstrap'
+ import React from 'react'
+
+ const MainHeader = ({header}) => (
+     <Jumbotron>
+       <h1> {header} </h1>
+     </Jumbotron>
+ )
+
+
+ export default MainHeader
